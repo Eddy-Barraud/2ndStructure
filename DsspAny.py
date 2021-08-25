f658c2d75a138e9604fbf716134f0bca8d76b254
Eddy Barraud <ebarraud@winad.priv>
2021-08-25 13:46:51 +0200

    Up

---
+++ DsspAny.py
@@ -0,0 +1,74 @@
+#https://www.mdtraj.org/1.9.5/api/generated/mdtraj.compute_dssp.html#mdtraj.compute_dssp
+# pyinstaller --onefile -w ".\DsspAny.py"
+import mdtraj as md
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.colors import LinearSegmentedColormap
+import os, re
+
+def doDssp(filename) :
+    traj = md.load(filename+".xtc",top=filename+".gro")
+    topology=traj.topology
+
+    dssp=md.compute_dssp(traj, simplified=True)
+
+    LidDsspMap={}
+    tick=0
+    for line in dssp :
+        translatedDssp=[]
+        for res in line:
+            if res == 'C':
+                translatedDssp.append(1)
+            elif res == 'H' :
+                translatedDssp.append(2)
+            elif res == 'E' :
+                translatedDssp.append(3)
+            else:
+                translatedDssp.append(0)
+        LidDsspMap[int(traj._time[tick])]=translatedDssp
+        tick+=1
+    
+    df=pd.DataFrame(data=LidDsspMap, index=None)
+
+    resIndex=[]
+    for i in range(0,topology._numResidues):
+        resIndex+=re.findall("[0-9]+",str(topology.residue(i)))
+    df.index = resIndex
+
+    dfT=df.transpose()
+
+    customCmap = LinearSegmentedColormap.from_list(
+        name='custom',
+        colors=['white','green','red']
+    )
+
+    plt.rcParams['figure.figsize'] = [8.0, 8.0]
+    plt.rcParams['figure.dpi'] = 400
+    plt.rcParams['savefig.dpi'] = 400
+
+    fig, ax = plt.subplots()
+    ax = sns.heatmap(dfT, vmin=1, vmax=3,cmap=customCmap, ax=ax)
+    ax.invert_yaxis()
+    ax.set_ylabel("Time (ps)")
+    ax.set_xlabel("Residue N°")
+    plt.savefig(filename+'.dssp.png')
+
+    #dfT2=df.loc[[str(i) for i in range(40,57)]].transpose()
+    #fig, ax = plt.subplots()
+    #ax = sns.heatmap(dfT2, vmin=1, vmax=3,cmap=customCmap, ax=ax)
+    #ax.invert_yaxis()
+    #ax.set_ylabel("Time (ps)")
+    #ax.set_xlabel("Residue N°")
+    #plt.savefig(filename+'.dssp.LID.png')
+
+
+    print("1: Coil or Undefined; 2: Helix; 3: Strand")
+    return True
+
+
+for root, dirs, files in os.walk(os.getcwd()):
+    for file in files:
+        if(file.endswith(".xtc") and not os.path.exists(os.path.splitext(file)[0]+".dssp.png")):
+            #print(os.path.join(root,file))
+            doDssp(os.path.splitext(file)[0])
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# 2ndStructure
-Plot the secondary structure of protein residues along a Molecular Dynamics simulation time. 
+<p align="center"><img width="500" alt="portfolio_view" src="./Example/try.dssp.png"></p>
+
+# Secondary Structure heatmap generator
+
+Author: Eddy BARRAUD
+
+Contact: <eddy.barraud@outlook.fr>
+
+Public access: [https://github.com/Eddy-Barraud/2ndStructure](https://github.com/Eddy-Barraud/2ndStructure)
+
+Requirements : pandas matplotlib seaborn mdtraj
+
+This script is calculating the secondary structure of each protein residue across simulation time with a library included DSSP tool.
+
+Generated heatmap values have these meanings :
+-   1: Coil or Undefined
+-   2: Helix
+-   3: Strand
+
+The python script behind the app is using [Seaborn](https://github.com/mwaskom/seaborn) and [MDTraj](https://github.com/mdtraj/mdtraj) libraries.
+
+1. Create files only for protein/residue of interest with GROMACS
+
+Example : 
+
+`gmx editconf -f npt_5.gro -n index.ndx -o try.gro`
+
+`gmx trjconv -f npt_5.xtc -s npt_5.tpr -n index.ndx -o try.xtc`
+
+2. Copy, under the same filename, the trajectory and the topology to the directory where the DssAny.py file is located at. See example files in the Example folder.
+
+3. Run the script.
+
+It will create a file, named filename.dssp.png.
